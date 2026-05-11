8668f07f3fdd4229b33b274a237087118cbc914a
pokemaniac101yt <pokemaniac101official@gmail.com>
2026-05-10 23:45:26 -0600

    Fabric 26.1 initial commit

---
+++ Fabric 26.1/src/main/java/net/pokemaniac/texturemod/TextureModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pokemaniac.texturemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TextureModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
