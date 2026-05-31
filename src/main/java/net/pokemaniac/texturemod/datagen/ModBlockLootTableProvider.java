5ee7b9aba374ba0fe6af6d16cff94d3e87b353eb
pokemaniac101yt <pokemaniac101official@gmail.com>
2026-05-31 00:01:25 -0600

    Removed preemptive ore loot table helper

--- src/main/java/net/pokemaniac/texturemod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/net/pokemaniac/texturemod/datagen/ModBlockLootTableProvider.java
@@ -27,20 +27,6 @@ public class ModBlockLootTableProvider extends FabricBlockLootSubProvider {
     @Override
     public void generate() {
         dropSelf(ModBlocks.COBBLESTONE_PRE_CLASSIC_RD_131655);
-
-//        add(ModBlocks.GOLD_ORE_CLASSIC_0_0_14a, createModOreDrop(ModBlocks.GOLD_ORE_0_0_14a, Items.RAW_GOLD, 1, 1);
     }
 
-    public LootTable.Builder createModOreDrops(final Block block, Item item, float minDrops, float maxDrops) {
-        HolderLookup.RegistryLookup<Enchantment> enchantments = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
-        return this.createSilkTouchDispatchTable(
-                block,
-                (LootPoolEntryContainer.Builder<?>)this.applyExplosionDecay(
-                        block,
-                        LootItem.lootTableItem(item)
-                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
-                                .apply(ApplyBonusCount.addOreBonusCount(enchantments.getOrThrow(Enchantments.FORTUNE)))
-                )
-        );
-    }
 }
