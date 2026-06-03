276a43a5501b73bec31338acf34f5246fb9baa97
pokemaniac101yt <pokemaniac101official@gmail.com>
2026-06-02 23:34:09 -0600

    Oak Pre-Classic rd-20090515 Pressure Plate and Button recipes

--- src/main/java/net/pokemaniac/texturemod/datagen/ModRecipeProvider.java
+++ src/main/java/net/pokemaniac/texturemod/datagen/ModRecipeProvider.java
@@ -101,6 +101,8 @@ public class ModRecipeProvider extends RecipeProvider {
                 .group("stone_buttons")
                 .save(output);
 
+
+        
         stairBuilder(ModBlocks.OAK_STAIRS_PRE_CLASSIC_RD_20090515, Ingredient.of(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
                 .unlockedBy(getHasName(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515), has(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
                 .group("wooden_stairs")
@@ -120,6 +122,16 @@ public class ModRecipeProvider extends RecipeProvider {
                 .unlockedBy(getHasName(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515), has(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
                 .group("wooden_fence_gate")
                 .save(output);
+
+        pressurePlateBuilder(RecipeCategory.REDSTONE, ModBlocks.OAK_PRESSURE_PLATE_PRE_CLASSIC_RD_20090515, Ingredient.of(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
+                .unlockedBy(getHasName(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515), has(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
+                .group("wooden_pressure_plate")
+                .save(output);
+
+        buttonBuilder(ModBlocks.OAK_BUTTON_PRE_CLASSIC_RD_20090515, Ingredient.of(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
+                .unlockedBy(getHasName(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515), has(ModBlocks.OAK_PLANKS_PRE_CLASSIC_RD_20090515))
+                .group("wooden_button")
+                .save(output);        
 
 
         /*
