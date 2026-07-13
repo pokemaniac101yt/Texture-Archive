e8f3451d04a40a2c7460ce067354def33f36d6fc
pokemaniac101yt <pokemaniac101official@gmail.com>
2026-07-12 22:10:54 -0600

    Lava blockmodels and datagen

--- src/main/java/net/pokemaniac/texturemod/datagen/ModModelProvider.java
+++ src/main/java/net/pokemaniac/texturemod/datagen/ModModelProvider.java
@@ -125,6 +125,7 @@ public class ModModelProvider extends ModelProvider {
 
         blockModels.createTrivialCube(ModBlocks.WATER_CLASSIC_0_0_12a.get());
         blockModels.createTrivialCube(ModBlocks.LAVA_CLASSIC_0_0_12a.get());
+        blockModels.createTrivialCube(ModBlocks.LAVA_CLASSIC_0_0_13a.get());
 
     }
 
